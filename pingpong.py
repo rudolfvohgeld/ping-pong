a6ffa2f46802d007a927db17788821e65625084b
rudolfvohgeld <159566125+rudolfvohgeld@users.noreply.github.com>
2024-02-16 20:36:41 +0300

    1
    chernovik cherny kak cherny

---
+++ pingpong.py
@@ -0,0 +1,96 @@
+from pygame import *
+from random import randint
+
+font.init()
+font1 = font.Font(None, 80)
+lose1 = font1.render('player 1 lose!', True, (255, 255, 255))
+lose2 = font1.render('player 2 lose', True, (180, 0, 0))
+
+speed_x=3
+speed_y=3
+
+game=True
+finish=False
+
+clock=time.Clock()
+FPS=60
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+    def fire(self):
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15)
+        bullets.add(bullet)
+
+back=(200,255,355)
+window=display.set_mode((600,500))
+window.fill(back)
+
+racket1=Player('racket.png',30,200,4,50,150)
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
+    if not finish:
+        window.blit(background,(0,0))
+        ship.update()
+        monsters.update()
+        bullets.update()
+        ship.reset()
+        monsters.draw(window)
+        bullets.draw(window)
+        collides = sprite.groupcollide(monsters, bullets, True, True)
+        for c in collides:
+            score = score + 1
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+            monsters.add(monster)
+        if sprite.spritecollide(ship, monsters, False) or lost >= max_lost:
+            finish = True 
+            window.blit(lose, (200, 200))
+        if score >= goal:
+            finish = True
+            window.blit(win, (200, 200))
+        text = font2.render("Счет: " + str(score), 1, (255, 255, 255))
+        window.blit(text, (10, 20))
+        text_lose = font2.render("Пропущено: " + str(lost), 1, (255, 255, 255))
+        window.blit(text_lose, (10, 50))
+        display.update()
+    else:
+        finish = False
+        score = 0
+        lost = 0
+        for b in bullets:
+            b.kill()
+        for m in monsters:
+            m.kill()
+        time.delay(3000)
+        for i in range(1, 6):
+            monster = Enemy(img_enemy, randint(80, win_width - 80), -40, 80, 50, randint(1, 5))
+            monsters.add(monster)
+
+
+    time.delay(50)
